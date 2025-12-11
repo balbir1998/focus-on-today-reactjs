e6393890bd84a7fe70467bd2946e1c1a0975563a
balbir1998 <122143810+balbir1998@users.noreply.github.com>
2025-12-11 14:53:29 +0530

    parent component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Main from './components/Main';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Focus on today</h1>
+      <Main />
     </>
   )
 }
 
-export default App
+export default App;
